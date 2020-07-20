6d10096b748f6d5d3dfd20c8d5fa7d9c1cdc8b07
JasonCheng1 <jasoncheng@Jasons-MBP.fios-router.home>
2020-07-20 00:07:34 -0400

    fixed some mistakes on last commit

--- app/schemas.py
+++ app/schemas.py
@@ -15,40 +15,52 @@ def ensure_feature_is_enabled(feature):
                 return return_resp(501, "Feature is disabled")
             else:
                 return return_resp(502, "Wrong Feature value")
+
         return wrapper
+
     return inner
 
 
-def ensure_json(*args, **kwargs):
+def ensure_json():
     validator = Draft4Validator({"type": "object"})
 
     def wrapper(fn):
         @wraps(fn)
-        def wrapped(*args, **kwargs):
+        def wrapped():
             test_input = request.get_json(force=True)
-            errors = [
-                error.message for error in validator.iter_errors(test_input)]
+            errors = [error.message for error in validator.iter_errors(test_input)]
             if errors:
                 return return_resp(505, "Invalid Json")
             else:
                 return fn()
+
         return wrapped
+
     return wrapper
 
 
-def ensure_user_logged_in(*args, **kwargs):
+def ensure_user_logged_in():
     def wrapper(fn):
         @wraps(fn)
-        def wrapped(*args, **kwargs):
+        def wrapped():
             data = request.get_json(force=True)
-            if not data or 'user_email' not in data or not data['user_email'] or 'token' not in data or not data['token']:
+            if (
+                not data
+                or "user_email" not in data
+                or not data["user_email"]
+                or "token" not in data
+                or not data["token"]
+            ):
                 return return_resp(408, "Missing email or token")
-            email = data['user_email']
-            #token = data['token']
+            email = data["user_email"]
+            token = data["token"]
+            # token = request.headers["token"] #TODO Get token from header
             email = email.strip().lower()
             if call_validate_endpoint(email, token) != 200:
                 return return_resp(404, "Invalid request")
             else:
                 return fn()
+
         return wrapped
+
     return wrapper
