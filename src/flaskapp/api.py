60d5414739f632846331633b5ee8e567451fcbae
v0lv0 <jindd8@gmail.com>
2020-08-01 19:50:30 -0400

    move data handling for teams into api.py

--- src/flaskapp/api.py
+++ src/flaskapp/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 
+from src.flaskapp.db import coll
 from src.users.user_profile import get_user_profile, create_user_profile
 from src.users.individual_recommendations import get_individual_recommendations
 from src.users.team_recommendations import get_team_recommendations
@@ -7,7 +8,7 @@ from src.users.interested import user_interested
 from src.users.leave_team import leave
 
 from src.teams.team_profile import get_team_profile
-from src.teams.open_teams import get_open_teams
+from src.teams.open_teams import return_open_teams
 from src.teams.start_a_team import create_team
 from src.teams.add_team_member import add_member
 from src.teams.confirm_member import confirm
@@ -18,9 +19,10 @@ from src.flaskapp.schemas import ensure_json, ensure_user_logged_in, ensure_feat
 
 app = Flask(__name__)
 
+
 @app.route("/user-profile", methods=["GET", "POST"])
 @ensure_json()
-@ensure_user_logged_in()
+# @ensure_user_logged_in()
 @ensure_feature_is_enabled("user profile")
 def user_profile():
     data = request.get_json(silent=True)
@@ -39,8 +41,24 @@ def user_profile():
 
 
 @app.route("/start-a-team", methods=["POST"])
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("start a team")
 def start_a_team():
-    return create_team()
+    data = request.get_json(silent=True)
+    email = data['user_email'].strip().lower()
+    if not data or 'name' not in data or 'desc' not in data or 'skills' not in data or not data['name'] \
+            or not data['desc'] or not data['skills']:
+        return {"statusCode": 400, "body": "Required info not found"}
+    team_name = data['name'].strip().lower()
+    team_desc = data['desc'].strip().lower()
+    partner_skills = data['skills']
+    formatted_skills = format_string(partner_skills)
+    formatted_prizes = []
+    if 'prizes' in data:
+        prizes = data['prizes']
+        formatted_prizes = format_string(prizes)
+    return create_team(team_name, email, team_desc, formatted_skills, formatted_prizes)
 
 
 @app.route("/leave-team", methods=["POST"])
@@ -54,24 +72,60 @@ def leave_team():
     return leave(email)
 
 
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("add team member")
 @app.route("/add-team-member", methods=["POST"])
 def add_team_member():
-    return add_member()
+    data = request.get_json(silent=True)
+    email = data['user_email'].strip().lower()
+    if not data or 'email' not in data or not data['email']:
+        return return_resp(400, "Required info not found")
+    partner_email = data['email'].strip().lower()
+    return add_member(email, partner_email)
 
 
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("team complete")
 @app.route("/team-complete", methods=["POST"])
 def team_complete():
-    return mark_team_complete()
+    data = request.get_json(silent=True)
+    email = data['user_email']
+    email = email.strip().lower()
+    team = coll("teams").find_one({"members": {"$all": [email]}})
+    if not team:
+        return return_resp(401, "User not in a team")
+    return mark_team_complete(team)
 
 
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("open teams")
 @app.route("/open-teams", methods=["GET"])
 def open_teams():
-    return get_open_teams()
+    data = request.get_json(silent=True)
+    if 'filter' not in data or not data['filter']:
+        search = None
+    else:
+        search = data['filter']
+
+    return return_open_teams(search)
 
 
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("team profile")
 @app.route("/team-profile", methods=["GET"])
 def team_profile():
-    return get_team_profile()
+    data = request.get_json(silent=True)
+    email = data["user_email"]
+    email = email.strip().lower()
+    team = coll("teams").find_one({"members": {"$all": [email]}})
+    if not team:
+        return return_resp(400, "Team Not found")
+    else:
+        return get_team_profile(team)
 
 
 @app.route("/team-recommendations", methods=["GET"])
@@ -110,6 +164,14 @@ def interested():
     return user_interested(email, name)
 
 
+@ensure_json()
+@ensure_user_logged_in()
+@ensure_feature_is_enabled("confirm member")
 @app.route("/confirm-member", methods=["POST"])
 def confirm_member():
-    return confirm()
+    data = request.get_json(silent=True)
+    email = data['user_email'].strip().lower()
+    if not data or 'email' not in data or not data['email']:
+        return return_resp(401, "Missing inf")
+    hacker = data['email'].strip().lower()
+    return confirm(email, hacker)
--- src/teams/confirm_member.py
+++ src/teams/confirm_member.py
@@ -4,25 +4,16 @@ from src.flaskapp.util import return_resp
 from src.flaskapp.db import coll
 from src.flaskapp.schemas import ensure_json, ensure_user_logged_in, ensure_feature_is_enabled
 
-@ensure_json()
-@ensure_user_logged_in()
-@ensure_feature_is_enabled("confirm member")
-def confirm():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        email = data['user_email']
-        email = email.strip().lower()
-        if not data or 'email' not in data or not data['email']:
-            return return_resp(401, "Missing inf")
-        hacker = data['email'].strip().lower()
-        team_name = coll("teams").find_one({"members": {"$all": [email]}}, {"_id"})['_id']
-        team = coll("teams").find_one({"_id": team_name})
-        team_members = team['members']
-        complete = coll("teams").find_one({"_id": team_name, "complete": True})
-        if len(team_members) >= 4 or complete:
-            return return_resp(402, "Team Complete")
-        coll("users").update_one({"_id": hacker}, {"$set": {"hasateam": True}})
-        coll("users").update_one({"_id": hacker}, {"$pull": {"potentialteams": team_name}})
-        coll("teams").update_one({"_id": team_name}, {"$push": {"members": hacker}})
-        coll("teams").update_one({"_id": team_name}, {"$pull": {"interested": hacker}})
-        return return_resp(200, "Success")
+
+def confirm(email, hacker):  # if request.method == 'POST'
+    team_name = coll("teams").find_one({"members": {"$all": [email]}}, {"_id"})['_id']
+    team = coll("teams").find_one({"_id": team_name})
+    team_members = team['members']
+    complete = coll("teams").find_one({"_id": team_name, "complete": True})
+    if len(team_members) >= 4 or complete:
+        return return_resp(402, "Team Complete")
+    coll("users").update_one({"_id": hacker}, {"$set": {"hasateam": True}})
+    coll("users").update_one({"_id": hacker}, {"$pull": {"potentialteams": team_name}})
+    coll("teams").update_one({"_id": team_name}, {"$push": {"members": hacker}})
+    coll("teams").update_one({"_id": team_name}, {"$pull": {"interested": hacker}})
+    return return_resp(200, "Success")
