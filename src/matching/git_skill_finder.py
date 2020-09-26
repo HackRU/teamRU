3409713d2f1d1d947d1b27d33d61a8f7e472b048
v0lv0 <jindd8@gmail.com>
2020-09-26 15:02:01 -0400

    recommendation updated, git language matcher added

--- src/matching/git_skill_finder.py
+++ src/matching/git_skill_finder.py
@@ -1,18 +1,19 @@
 from github import Github
 
-# def get_git_skills( username ):
 
-g = Github()
-user = g.get_user("v0lv0")
+def get_git_skills(username):
+    g = Github()
+    user = g.get_user(username)
 
-languages = {}
-for repo in user.get_repos():
-    new_repo_languages = repo.get_languages()
-    for key in new_repo_languages:
-        if key in languages:
-            languages[key] = languages[key] + new_repo_languages[key]
-        else:
-            languages[key] = new_repo_languages[key]
+    languages = {}
+    for repo in user.get_repos():
+        new_repo_languages = repo.get_languages()
+        for key in new_repo_languages:
+            if key in languages:
+                languages[key] = languages[key] + new_repo_languages[key]
+            else:
+                languages[key] = new_repo_languages[key]
 
-languages = sorted(languages.items(), key=lambda x: [1], reverse=True)
-print(languages)
+    languages = sorted(languages.items(), key=lambda x: [1], reverse=True)
+    # print(languages)
+    return languages
