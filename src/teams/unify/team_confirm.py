2ec3e576ed77676961490bd26af786292d0003a1
anitejb <41495799+anitejb@users.noreply.github.com>
2020-09-29 17:36:27 -0400

    fix for docstring, update_one in team_confirm

--- src/teams/unify/team_confirm.py
+++ src/teams/unify/team_confirm.py
@@ -5,11 +5,14 @@ from src.flaskapp.util import aggregate_team_meta
 def team_confirm(email, team1_name, team2_name):  # if request.method == 'POST'
     """Confirming an invitation from a team (i.e. team1 -confirms-> team2)
 
+    team1 is the team which is receiving and confirming the invite
+    team2 is the team which made the original invite
+
     Performs checks to see if the merging can be carried out still (checks include: 1) new team
     size is not > 4 2) no rescinds or rejects beforehand which would have nullified the invite)
 
-    If checks pass then team2 is merged with team1 by adding team2's member(s) into team1's member
-    list and setting team1 to complete if the new team size is 4
+    If checks pass then team1 (confirming team) is merged with team2 (original team) by adding 
+    team1's member(s) into team2's member list and setting team2 to complete if the new team size is 4
 
     Args:
         team1_name: name of the team that is confirming the invite (invitee)
@@ -45,8 +48,10 @@ def team_confirm(email, team1_name, team2_name):  # if request.method == 'POST'
         {
             "$push": {"members": {"$each": team1["members"]}},
             "$pull": {"outgoing_inv": team1_name},
-            "$set": {"complete": new_length == 4},
-            "$set": {"meta": aggregate_team_meta(team1["members"] + team2["members"])},
+            "$set": {
+                "complete": new_length == 4,
+                "meta": aggregate_team_meta(team1["members"] + team2["members"]),
+            },
         },
     )
     coll("teams").delete_one({"_id": team1_name})
