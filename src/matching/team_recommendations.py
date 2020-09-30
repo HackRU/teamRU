00afbdc02f51bfe6305ac0aa2676d757d699d8a0
v0lv0 <jindd8@gmail.com>
2020-09-30 13:19:58 -0400

    some comment added

--- src/matching/team_recommendations.py
+++ src/matching/team_recommendations.py
@@ -10,8 +10,9 @@ def get_team_recommendations(email):  # GET
         email: the email of the individual that wants recommendations of teams to join
 
     Return:
-        a list of recommmended teams to join
+        a list of recommended teams to join
     """
+
     user = coll("users").find_one({"_id": email})
     if not user:
         return {"message": "Invalid user"}, 403
@@ -19,44 +20,56 @@ def get_team_recommendations(email):  # GET
     if user_in_a_team:
         return {"message": "User in a team"}, 402
     if "skills" not in user or not user["skills"]:
-        return {"message": "No recommendations found"}, 400
+        skills = []
+    else:
+        skills = user["skills"]
     if "prizes" not in user or not user["prizes"]:
         prizes = []
     else:
         prizes = user["prizes"]
-        skills = user["skills"]
         names = set()
 
     matches = []
     for skill in skills:
+        # collection of all the team's skills
         complementary_skills_match = coll("teams").aggregate(
             [{"$match": {"complete": False, "partnerskills": {"$all": [skill]}}}]
         )
+        # collections of all the team's interests
         interests_match = coll("teams").aggregate(
             [{"$match": {"complete": False, "interested": {"$all": [skill]}}}]
         )
 
-        if not complementary_skills_match or interests_match:
+        if not complementary_skills_match or not interests_match:
             continue
+
         for this_interest in interests_match:
             if this_interest["_id"] not in names:
                 names.add(this_interest["_id"])
                 matches.append(this_interest)
 
-
+        # add suggestion base on skills
         frontend_languages = ["HTML", "CSS", "JavaScript"]
         backend_languages = ["Java", "PHP", "Ruby", "Python", "c", "c++"]
-
-
         front_or_back = "none"
+
+        # give backend suggestions if only know frontend, vice versa
         for original_skill in complementary_skills_match:
             if original_skill in frontend_languages:
-                front_or_back = "front"
+                if front_or_back == "back":
+                    front_or_back = "none"
+                    break
+                else:
+                    front_or_back = "front"
             if original_skill in backend_languages:
-                front_or_back = "back"
+                if front_or_back == "front":
+                    front_or_back = "none"
+                    break
+                else:
+                    front_or_back = "front"
 
         if front_or_back == "front":
-            complementary_skills_match.append( frontend_languages )
+            complementary_skills_match.append(frontend_languages)
         if front_or_back == "back":
             complementary_skills_match.append(backend_languages)
 
@@ -65,6 +78,7 @@ def get_team_recommendations(email):  # GET
                 names.add(this_interest["_id"])
                 matches.append(this_interest)
 
+    # add suggestions base on prize
     for prize in prizes:
         match = coll("teams").aggregate(
             [{"$match": {"complete": False, "prizes": {"$all": [prize]}}}]
@@ -76,6 +90,7 @@ def get_team_recommendations(email):  # GET
                 names.add(m["_id"])
                 matches.append(m)
 
+    # return
     if not matches:
         return {"message": "No recommendations found"}, 400
     return {"matches": matches}, 200
