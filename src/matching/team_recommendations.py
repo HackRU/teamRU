6016e1b90f4a9e326defe1328a748d2bb6594158
v0lv0 <jindd8@gmail.com>
2020-10-31 13:28:30 -0400

    changed the way skills are being added. no more nested for loops

--- src/matching/team_recommendations.py
+++ src/matching/team_recommendations.py
@@ -16,57 +16,38 @@ def get_team_recommendations(email):  # GET
 
     user = coll("users").find_one({"_id": email})
     if not user:
-        return {"message": "Invalid user"}, 403
-    user_in_a_team = coll("users").find_one({"_id": email, "hasateam": True})
-    if user_in_a_team:
+        return {"message": "Invalid user"},
+
+    if user["hasateam"]:
         return {"message": "User in a team"}, 400
     # basic info about users
-    if "skills" not in user or not user["skills"]:
-        skills = []
-    else:
-        skills = user["skills"]
-    if "interests" not in user or not user["interests"]:
-        interests = []
-    else:
-        interests = user["interests"]
-    if "prizes" not in user or not user["prizes"]:
-        prizes = []
-    else:
-        prizes = user["prizes"]
-    if "seriousness" not in user or not user["seriousness"]:
-        seriousness = 0
-    else:
-        seriousness = user["seriousness"]
+    skills = user["skills"]
+    interests = user["interests"]
+    prizes = user["prizes"]
+    seriousness = user["seriousness"]
 
     names = set()
     matches = []
 
     # match for skill
     needed_skills = []
-    # judging if the user if frontend or backend
-    frontend_languages = ["html", "css", "javascript", "php", "typscript"]
-    backend_languages = ["java", "php", "ruby", "python", "c", "c++", "sql", "node.js"]
-    front_or_back = "none"
-    for original_skill in skills:
-        original_skill = original_skill.lower()
-        if original_skill in frontend_languages:
-            if front_or_back == "back":
-                front_or_back = "none"
-                break
-            else:
-                front_or_back = "front"
 
-        if original_skill in backend_languages:
-            if front_or_back == "front":
-                front_or_back = "none"
-                break
-            else:
-                front_or_back = "front"
-    # give backend suggestions if only know frontend, vice versa
-    if front_or_back == "front":
-        needed_skills.append(frontend_languages)
-    if front_or_back == "back":
+    frontend_languages = set(["html", "css", "javascript", "php", "typscript"])
+    backend_languages = set(["java", "php", "ruby", "python", "c", "c++", "sql", "node.js"])
+    # judging if the user if frontend or backend, and give backend suggestions if only know frontend, vice versa
+    skill_set = set(skills)
+    front_num = len(skill_set.intersection(skills))
+    back_num = len(skill_set.intersection(skills))
+
+    if front_num > (back_num * 5/8):
+        if back_num < 3:
+            needed_skills.append(backend_languages)
+    else:
+        if front_num < 3:
+            needed_skills.append(frontend_languages)
+    if len(needed_skills):
         needed_skills.append(backend_languages)
+        needed_skills.append(frontend_languages)
 
     for skill in needed_skills:
         # collection of all the team's skills
