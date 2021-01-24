e2f4268b5618d76aab49cf5605c78aa6e10857e2
JasonCheng1 <stealthyjason@gmail.com>
2021-01-24 12:40:42 -0500

    removed some parenthesis

--- src/teams/unify/team_invite.py
+++ src/teams/unify/team_invite.py
@@ -19,24 +19,24 @@ def team_invite(email, team1_id, team2_id):  # POST
     team2 = coll("teams").find_one({"_id": team2_id})
 
     if not team1 or not team2:
-        return ({"message": "Invalid team id(s)"}, 404)
+        return {"message": "Invalid team id(s)"}, 404
 
     if email not in team1["members"]:
-        return ({"message": f"User not in team {team1_id}"}, 403)
+        return {"message": f"User not in team {team1_id}"}, 403
 
     if team1_id == team2_id:  # check to see if you are sending invite to yourself
-        return ({"message": "Cannot invite your own team"}, 400)
+        return {"message": "Cannot invite your own team"}, 400
 
     if (
         team1_id in team2["incoming_inv"]
     ):  # (team2_id in team1["outgoing_inv"] will also work) check to see if you are sending a duplicate invite
-        return ({"message": "Cannot have duplicate invite"}, 400)
+        return {"message": "Cannot have duplicate invite"}, 400
 
     if len(team1["members"]) + len(team2["members"]) > 4:
-        return ({"message": "Team size will be greater than 4"}, 409)
+        return {"message": "Team size will be greater than 4"}, 409
 
     if team1["complete"] or team2["complete"]:
-        return ({"message": "Team already complete "}, 409)
+        return {"message": "Team already complete "}, 409
 
     coll("teams").update_one({"_id": team1_id}, {"$push": {"outgoing_inv": team2_id}})
     coll("teams").update_one({"_id": team2_id}, {"$push": {"incoming_inv": team1_id}})
@@ -45,4 +45,4 @@ def team_invite(email, team1_id, team2_id):  # POST
         subject="Pending TeamRU Invite",
         body="Go to https://hackru.org/ to accept the invite",
     )
-    return ({"message": "Success"}, 200)
+    return {"message": "Success"}, 200
