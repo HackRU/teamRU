5f62a83c95ca292a8777375cbd2c67104aa89504
v0lv0 <jindd8@gmail.com>
2022-03-09 10:29:03 -0500

    syntax fix

--- src/forum/forum.py
+++ src/forum/forum.py
@@ -1,34 +1,6 @@
-# Forum API (all uuids are universal)
-# 	-> need to get all forum posts by a given user
-# 		-> payload is just their email in the query string e.g. ‘${api-route}/${email}’
-# 		-> return {poster : string, title : string, uuid : string}[]
-# 
-# 	-> need to POST a forum post
-# 		-> payload {poster : string, title : string, content : string} 
-#     and will be sent in the body of the request. The token 
-#     will be in the header
-# 		-> return the uuid generated for that post
-# 
-# 	-> need to POST a comment
-# 		-> payload {poster : string, content : string, parent_uuid : string}
-# 		    and will be sent in the request body. The token will be in the header
-# 		-> return {poster : string, content : string, uuid : string}
-# 
-#   -> need to get the forum post itself
-# 		-> payload is the post uuid e.g. ‘${api-route}/${uuid}’
-# 		-> return {poster : string, title : string, content : string
-# 	-> need to get all comments of a post
-# 		-> payload is the post uuid e.g. ‘${api-route}/${uuid}’
-# 		-> return {poster : string, content : string, uuid : string, 
-#     subcomments : comment[]}[] //an array of comments where 
-#     each comment itself has a field ‘subcomments’ that is an array 
-#     of comments that have that former comment as its parent
-
 from src.flaskapp.db import coll
 from pymongo import ObjectId
 
-# need to get all forum posts posted (maybe only for current year)
-#   return {poster : string, title : string, uuid : string}[]
 def get_all_posts(limit=100):
     posts = list(coll("posts").find({}).limit(limit))
     return {"posts": posts}, 200
@@ -37,6 +9,7 @@ def get_all_posts(limit=100):
 def get_user_posts(poster, limit=100):
     posts = list(coll("posts").find({"poster": poster}).limit(limit))
     return {"posts": posts}, 200
+
 
 def get_all_comments(parent_id):
     post = coll("posts").find_one({"_id": parent_id})
@@ -48,6 +21,7 @@ def get_all_comments(parent_id):
         comments.append(coll("comments").find_one({"_id": comment_id}))
 
     return {"comments": comments}, 200
+
 
 def get_all_sub_comments(parent_id):
     comment = coll("comments").find_one({"_id": parent_id})
@@ -61,11 +35,7 @@ def get_all_sub_comments(parent_id):
 
     return {"subcomments": subcomments}, 200
 
-# 	need to POST a forum post
-# 	    payload {poster : string, title : string, content : string} 
-#       and will be sent in the body of the request. The token 
-#   will be in the header
-# 		-> return the uuid generated for that post
+
 def post_post(**kwargs):
     id = ObjectId()
     coll("posts").insert_one(
@@ -73,15 +43,16 @@ def post_post(**kwargs):
             "_id": id,
             "poster": kwargs["poster"],
             "title": kwargs["title"],
-            "content": kwargs["content"]
+            "content": kwargs["content"],
             "comment": []
         }
     )
     return {"post_id": id}, 200
 
+
 def post_comment(**kwargs):
     id = ObjectId()
-    parent = coll("posts").find_one({"_id": email})
+    parent = coll("posts").find_one({"_id": kwargs["parent_uuid"]})
     if not parent:
         return {"message": "post not found"}, 404
     coll("posts").update_one({'_id': kwargs["parent_uuid"]},{'$push': {"comment": id}})
@@ -95,14 +66,15 @@ def post_comment(**kwargs):
     )
     return {"comment_id": id}, 200
 
+
 def post_sub_comment(**kwargs):
     id = ObjectId()
 
-    parent = coll("comment").find_one({"_id": email})
+    parent = coll("comment").find_one({"_id": kwargs["parent_uuid"]})
     if not parent:
         return {"message": "comment not found"}, 404
     coll("comment").update_one({'_id': kwargs["parent_uuid"]},{'$push': {"subcomment": id}})
-
+    
     coll("subcomments").insert_one(
         {
             "_id": id,
@@ -110,5 +82,4 @@ def post_sub_comment(**kwargs):
             "content": kwargs["content"]
         }
     )
-
     return {"sub_comment_id": id}, 200
